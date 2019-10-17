cb5d617da0e3a15f8fce6c7e34b16c391ac6a673
Fredrik Vold <fredrik@webkonsept.com>
2019-10-17 15:45:52 +0200

    Very first basic octree usage

--- cl_uteknark.lua
+++ cl_uteknark.lua
@@ -2,6 +2,8 @@ local table = table
 local plantingTargetOffset = vector3(0,2,-3)
 local rayFlagsLocation = 17
 local rayFlagsObstruction = 273
+local octree = pOctree(vector3(0,1500,0),vector3(12000,12000,2000)) -- Covers the whole damn map!
+local activePlants = {}
 
 function flatEnough(surfaceNormal)
     local x = math.abs(surfaceNormal.x)
@@ -77,13 +79,97 @@ end
 Citizen.CreateThread(function()
     while true do
         if debug.active then
+            --[[
             local plantable, message, where, normal, material = getPlantingLocation(true)
             if not plantable and message then
                 debug(_U(message))
             end
+            ]]
             debug:flush()
         end
         Citizen.Wait(0)
+    end
+end)
+
+
+function DrawIndicator(location, r, g, b, a)
+    local range = Config.Distance.Interact * 0.5
+    DrawMarker(
+        23, -- type (23 is a fat horizontal ring)
+        location + vector3(0,0,0.2),
+        0.0, 0.0, 0.0, -- direction (?)
+        0.0, 0.0, 0.0, -- rotation
+        range, range, range, -- scale
+        r, g, b, a, -- color
+        true, -- bob
+        false, -- face camera
+        2, -- dunno, lol, 100% cargo cult
+        false, -- rotates
+        0, 0, -- texture
+        false -- Projects/draws on entities
+    )
+end
+
+Citizen.CreateThread(function()
+    local drawDistance = Config.Distance.Draw
+    -- drawDistance = 10 -- For testing purposes
+    drawDistance = drawDistance * 1.1 -- So they don't fight about it, culling is at a slightly longer range
+    while true do
+        local waitAt = math.floor(math.sqrt(#activePlants))
+        local myLocation = GetEntityCoords(PlayerPedId())
+        local closestDistance
+        local closestPlant
+        for i,plant in ipairs(activePlants) do
+            local distance = #(plant.at - myLocation)
+            if distance > drawDistance then
+                DeleteObject(plant.object)
+                plant.node.data.object = nil
+                table.remove(activePlants, i)
+            elseif not closestDistance or distance < closestDistance then
+                closestDistance = distance
+                closestPlant = plant
+            end
+        end
+        if #activePlants > 0 then
+            debug(#activePlants,'active plants')
+            debug('Closest plant at',closestDistance,'meters')
+            if closestDistance <= Config.Distance.Interact then
+                DrawIndicator(closestPlant.at, 0, 255, 0, 128)
+                debug('Within intraction distance!')
+            end
+            Citizen.Wait(0)
+        else
+            Citizen.Wait(500)
+        end
+    end
+end)
+
+Citizen.CreateThread(function()
+    local drawDistance = Config.Distance.Draw
+    -- drawDistance = 10 -- For testing purposes
+    while true do
+        local here = GetEntityCoords(PlayerPedId())
+        local hits = octree:searchSphere(here, drawDistance)
+        for i,entry in ipairs(hits) do
+            if not entry.data.object then
+                local stage = entry.data.stage or 1
+                local model = Growth[stage].model
+                local weed = CreateObject(model, entry.bounds.location, false, false, false)
+                local heading = math.random(0,359) * 1.0
+                SetEntityHeading(weed, heading)
+                FreezeEntityPosition(weed, true)
+                SetEntityCollision(weed, false, true)
+                SetEntityLodDist(weed, drawDistance)
+                table.insert(activePlants, {node=entry, object=weed, at=entry.bounds.location})
+                entry.data.object = weed
+            end
+        end
+        if #hits > 0 then
+            debug(#hits,'octree hits')
+            Citizen.Wait(0)
+        else
+            Citizen.Wait(500)
+        end
     end
 end)
 
@@ -91,3 +177,67 @@ RegisterCommand('groundmat',function(source, args, raw)
     local plantable, message, where, normal, material = getPlantingLocation(true)
     TriggerEvent("chat:addMessage", {args={'Material', material}})
 end, false)
+
+function deleteActivePlants()
+    for i,plant in ipairs(activePlants) do
+        if DoesEntityExist(plant.object) then
+            DeleteObject(plant.object)
+        end
+    end
+    activePlants = {}
+end
+
+local testWeedModel = `prop_weed_02`
+function testWeed(location)
+    if not HasModelLoaded(testWeedModel) then
+        RequestModel(testWeedModel)
+        while not HasModelLoaded(testWeedModel) do
+            Citizen.Wait(0)
+        end
+    end
+    local found, Z = GetGroundZFor_3dCoord(location.x, location.y, location.z, false)
+    if found then
+        local weed = CreateObject(testWeedModel, location.x, location.y, Z, false, false, false)
+        FreezeEntityPosition(weed, true)
+        SetEntityCollision(weed, false, true)
+        return weed
+    end
+end
+
+RegisterCommand('testforest', function(source, args, raw)
+    local origin = GetEntityCoords(PlayerPedId())
+    local count = 25
+    if args[1] and string.match(args[1],'^[0-9]+$') then
+        count = tonumber(args[1])
+    end
+    TriggerEvent("chat:addMessage", {args={'Forest size', count}})
+    local column = math.ceil(math.sqrt(count))
+    TriggerEvent("chat:addMessage", {args={'Column', column}})
+    local offset = (column * Config.Distance.Space)/2
+    offset = vector3(-offset, -offset, 5)
+    local cursor = origin + offset
+    local planted = 0
+    while planted < count do
+        --[[
+        local weed = testWeed(cursor)
+        table.insert(activePlants,{object=weed,at=cursor})
+        ]]
+        local found, Z = GetGroundZFor_3dCoord(cursor.x, cursor.y, cursor.z, false)
+        if found then
+            octree:insert(vector3(cursor.x, cursor.y, Z), 0.2, {stage=1})
+        end
+        cursor = cursor + vector3(0, Config.Distance.Space, 0)
+        planted = planted + 1
+        if planted % column == 0 then
+            Citizen.Wait(0)
+            cursor = cursor + vector3(Config.Distance.Space, -(Config.Distance.Space * column), 0)
+        end
+
+    end
+end, false)
+
+AddEventHandler('onResourceStop', function(resourceName)
+    if resourceName == GetCurrentResourceName() then
+        deleteActivePlants()
+    end
+end)
