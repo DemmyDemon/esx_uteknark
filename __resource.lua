6571801ad2cbb35d5a69a7f46578601a76cb0028
Fredrik Vold <fredrik@webkonsept.com>
2019-10-24 18:50:50 +0200

    Breze seems unlikely to contribute :/

--- __resource.lua
+++ __resource.lua
@@ -1,5 +1,5 @@
 resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
-description 'ESX UteKnark by DemmyDemon and Breze'
+description 'ESX UteKnark by DemmyDemon'
 
 dependencies {'es_extended','mysql-async'}
 
