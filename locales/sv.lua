5e41c3a4e758e575387d2b616bb6050fb74b6bc0
Fredrik Vold <fredrik@webkonsept.com>
2019-11-06 20:18:48 +0100

    Updated Swedish translation

--- locales/sv.lua
+++ locales/sv.lua
@@ -1,10 +1,29 @@
 Locales['sv'] = {
     planting_text = 'Plantering',
-    planting_ok = 'Planterad',
-    planting_in_vehicle = 'Plantera från fordon? Seriöst?',
-    planting_too_steep = 'Lite för brant där, va?',
-    planting_too_far = 'Gå lite närmre.',
-    planting_not_suitable_soil = 'Där växer det nog dåligt.',
-    planting_too_close = 'För nära andra växter.',
-    planting_obstructed = 'Hitta någonstans med mer ljus.',
+    planting_ok = 'Lyckad plantering!',
+    planting_in_vehicle = 'Slänga frön ur fönstret, eller vad?',
+    planting_too_steep = 'För brant, kompis!',
+    planting_too_far = 'Lite närmre, kanske?',
+    planting_not_suitable_soil = 'Här kommer det växa för dåligt.',
+    planting_too_close = 'För nära en annan växt!',
+    planting_obstructed = 'Hitta någonstans med mera ljus',
+    planting_no_seed = 'Du har inga frön',
+    planting_failed = '~r~Jävlar! Något skit hände under planteringen!',
+
+    status_active = 'Steg ~1~/~1~~n~~INPUT_PICKUP~ ~a~~n~~INPUT_COVER~ Förstör',
+    status_passive = 'Steg ~1~/~1~~n~~a~~n~~INPUT_COVER~ Förstör',
+
+    growth_seedling = 'Nyligen planterat.~n~Behöver vårtd snart!',
+    growth_tend = 'Vårda växten',
+    growth_growing = 'Växer och mår bra.',
+    growth_yield = 'Skörda',
+
+    command_invalid = 'Ogiltigt direktiv: %s',
+    command_empty = 'Ja?',
+
+    interact_text = 'Växtinteraktion',
+    interact_tended = 'Växt vårdad',
+    interact_destroyed = 'Växt förstörd',
+    interact_harvested = 'Växt skördad: %i gräs, %i frön',
+    interact_full = 'Du har inte plats med %i mer gräs!',
 }
